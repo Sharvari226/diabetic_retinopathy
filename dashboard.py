08f6f3b4a7694431b0017275d5a5fd534a334b6f
Sharvari226 <sharvarijadhav2206@gmail.com>
2025-10-29 00:04:56 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,3 @@
-import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.layers import Lambda
-
-@st.cache_resource
-def load_trained_model():
-    model = tf.keras.models.load_model("dr_multitask_v2_final", compile=False)
-    return model
 
 
-model = load_trained_model()
-st.sidebar.success("✅ Model Loaded Successfully")
 
